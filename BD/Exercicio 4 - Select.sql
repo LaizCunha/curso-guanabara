b8cdac5ceb43e701aa44850cc28d9e457c69e90c
Laiz Cunha <laizsc.89@gmail.com>
2023-08-11 11:54:12 -0300

    Exercicio 4 - Select

---
+++ BD/Exercicio 4 - Select.sql
@@ -0,0 +1,53 @@
+select carga, count(*) from cursos
+group by carga;
+
+select totaulas, count(*) from cursos
+where totaulas > 20
+group by totaulas;
+
+select carga, count(*) from cursos
+group by carga
+having count(*) > 3;
+
+select ano, count(*) from cursos
+group by ano
+having count(ano) >= 5
+order by count(*) desc;
+
+select ano, count(*) from cursos
+where otaulas > 30
+group by ano
+having count(ano) >= 5
+order by count(*) desc;
+
+select avg(carga) from cursos;
+
+select carga, count(*) from cursos
+where ano > 2015
+group by carga;
+
+select carga, count(*) from cursos
+where ano > 2015
+group by carga
+having carga > (select avg(carga) from cursos);
+
+
+select profissao, count(*) from gafanhotos
+group by profissao;
+
+select sexo, count(*) from gafanhotos
+where nascimento > '2005-01-01'
+group by sexo;
+
+select nacionalidade, count(*) from gafanhotos
+where nacionalidade != 'Brasil'
+group by nacionalidade
+having count(nacionalidade) > 3;
+
+select altura, count(*) from gafanhotos
+where peso > '100'
+group by altura
+having altura > (select avg(altura) from gafanhotos);
+
+select avg(altura) from gafanhotos;
+select * from gafanhotos;
